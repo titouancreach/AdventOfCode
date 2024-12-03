0e7340a3fd5b8e3d2d9b78a1985c9467c269498e
Titouan CREACH <t.creach@unitee.io>
2024-12-03 09:40:38 +0100

    feat: p1

---
+++ 2024/Day3/Program.fs
@@ -0,0 +1,22 @@
+﻿open Day2.Parser.Parser
+open Day2.Parser
+
+// let input =
+// "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+let input = System.IO.File.ReadAllText("input.txt")
+
+let mulParser =
+    pstring "mul" >>. pchar '(' >>. manyDigit .>> pchar ',' .>>. manyDigit
+    .>> pchar ')'
+
+let parser = many (skipManyTill anyChar mulParser)
+
+let result = run parser input
+
+let evaluate =
+    function
+    | Success(value, _) -> Ok(value |> List.map (fun (a, b) -> a * b) |> List.sum)
+    | Failure(msg) -> Result.Error msg
+
+printfn "%A" <| evaluate result
