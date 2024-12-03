2c1b1b0ebd1d0d0887ab18b97b26b61129a47e5d
Titouan CREACH <t.creach@unitee.io>
2024-12-03 15:03:13 +0100

    day3 part 2

--- 2024/Day3/Program.fs
+++ 2024/Day3/Program.fs
@@ -4,19 +4,72 @@ open Day2.Parser
 // let input =
 // "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
+type Token =
+    | Mul of int * int
+    | Do
+    | Dont
+
 let input = System.IO.File.ReadAllText("input.txt")
 
 let mulParser =
     pstring "mul" >>. pchar '(' >>. manyDigit .>> pchar ',' .>>. manyDigit
     .>> pchar ')'
+    |>> (fun (a, b) -> Mul(a, b))
 
-let parser = many (skipManyTill anyChar mulParser)
+let doParser = pstring "do()" |>> (fun _ -> Do)
+let dontParser = pstring "don't()" |>> (fun _ -> Dont)
+
+let parser =
+    many (skipManyTill anyChar (choice [ mulParser; dontParser; doParser ]))
 
 let result = run parser input
 
-let evaluate =
+let evaluatePartOne =
     function
-    | Success(value, _) -> Ok(value |> List.map (fun (a, b) -> a * b) |> List.sum)
-    | Failure(msg) -> Result.Error msg
+    | Success(x, _) ->
+        Ok(
+            List.map
+                (function
+                | Mul(a, b) -> Some(a * b)
+                | _ -> None)
 
-printfn "%A" <| evaluate result
+                x
+        )
+    | _ -> Error("Failed to parse")
+
+
+let removeDont (tokens: Token list) : Token list =
+    let rec loop tokens enabled acc =
+        match tokens with
+        | [] -> List.rev acc
+        | Dont :: tail -> loop tail false acc
+        | Do :: tail -> loop tail true acc
+        | Mul(a, b) :: tail when enabled -> loop tail enabled (Mul(a, b) :: acc)
+        | _ :: tail -> loop tail enabled acc
+
+    loop tokens true []
+
+
+let evaluatePartTwo =
+    function
+    | Success(x, _) ->
+        Ok(
+            x
+            |> removeDont
+            |> List.map (function
+                | Mul(a, b) -> Some(a * b)
+                | _ -> None)
+
+        )
+    | _ -> Error("Failed to parse")
+
+
+
+let x =
+    evaluatePartOne result |> Result.map (List.choose id) |> Result.map (List.sum)
+
+let y =
+    evaluatePartTwo result |> Result.map (List.choose id) |> Result.map (List.sum)
+
+printfn "%A" x
+printfn "%A" y
