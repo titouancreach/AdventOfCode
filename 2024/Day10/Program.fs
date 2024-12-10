a08b9fc495f0dfff22cea8c3a5a6af7116719714
Titouan CREACH <t.creach@unitee.io>
2024-12-10 11:07:27 +0100

    feat: day10

---
+++ 2024/Day10/Program.fs
@@ -0,0 +1,81 @@
+﻿open System.Collections.Generic
+
+let inputEx1 =
+    [| "...0..."
+       "...1..."
+       "...2..."
+       "6543456"
+       "7.....7"
+       "8.....8"
+       "9.....9" |]
+
+let inputEx2 =
+    [| "..90..9"
+       "...1.98"
+       "...2..7"
+       "6543456"
+       "765.987"
+       "876...."
+       "987...." |]
+
+let largerInput =
+    [| "89010123"
+       "78121874"
+       "87430965"
+       "96549874"
+       "45678903"
+       "32019012"
+       "01329801"
+       "10456732" |]
+
+let puzzleInput = System.IO.File.ReadAllLines "input.txt"
+
+let parse (input: string array) =
+    input |> Array.map _.ToCharArray() |> array2D
+
+let findTrailHead (map: char[,]) =
+    seq {
+        for i in 0 .. map.GetLength(0) - 1 do
+            for j in 0 .. map.GetLength(1) - 1 do
+                if map[i, j] = '0' then
+                    yield (i, j)
+    }
+
+let tryGetChar (map: char[,]) ((i, j)) =
+    if i >= 0 && i < map.GetLength(0) && j >= 0 && j < map.GetLength(1) then
+        Some map[i, j]
+    else
+        None
+
+let solve (map: char[,]) (trailHead: (int * int)) =
+    let visitedNines = HashSet<int * int>()
+
+    let rec aux currentCount (currentPos: int * int) (path: (int * int) list) =
+        match tryGetChar map currentPos with
+        | Some '9' when currentCount = 9 -> //&& not (visitedNines.Contains(currentPos)) ->
+            visitedNines.Add(currentPos) |> ignore
+            1
+        | Some c when int (c - '0') = currentCount ->
+            let left =
+                aux (currentCount + 1) (fst currentPos, snd currentPos - 1) (currentPos :: path)
+
+            let right =
+                aux (currentCount + 1) (fst currentPos, snd currentPos + 1) (currentPos :: path)
+
+            let up =
+                aux (currentCount + 1) (fst currentPos - 1, snd currentPos) (currentPos :: path)
+
+            let down =
+                aux (currentCount + 1) (fst currentPos + 1, snd currentPos) (currentPos :: path)
+
+            left + right + up + down
+
+        | _ -> 0
+
+    aux 0 trailHead []
+
+let map = parse puzzleInput
+
+let resultPart1 = map |> findTrailHead |> Seq.map (solve map) |> Seq.sum
+
+printfn "%A" resultPart1
--- 2024/Day9/Program.fs
+++ 2024/Day9/Program.fs
@@ -12,6 +12,11 @@ type Bloc =
     | Filled of FilledRecord
     | Free of int
 
+    override this.ToString() : string =
+        match this with
+        | Filled({ count = count; id = Id(id) }) -> sprintf "Filled(%d)*%d" count id
+        | Free(n) -> sprintf "Free*%d" n
+
 let parse (input: string) =
     input
     |> Seq.mapi (fun i c ->
@@ -23,6 +28,20 @@ let parse (input: string) =
                   id = Id(i / 2) }
             ))
     |> Seq.toList
+
+
+
+
+let countAll (fileSystem: Bloc list) =
+    seq {
+        for (i, bloc) in fileSystem |> Seq.indexed do
+            let subSeq =
+                match bloc with
+                | Filled({ count = count; id = Id(id) }) -> Seq.replicate count id
+                | Free(n) -> Seq.replicate n 0
+
+            yield! subSeq
+    }
 
 
 let rec part1 (fileSystem: Bloc list) =
@@ -81,29 +100,118 @@ let rec part1 (fileSystem: Bloc list) =
             @ maybeRemainingFilled
             @ fileSystem[filledIndex + 1 ..]
 
+
         part1 newFileSystem
 
     | _ -> fileSystem
 
 
-let countAll (fileSystem: Bloc list) =
-    seq {
-        for (i, bloc) in fileSystem |> Seq.indexed do
-            let subSeq =
-                match bloc with
-                | Filled({ count = count; id = Id(id) }) -> seq { for _ in 1..count -> id }
-                | Free(_) -> Seq.empty
-
-            yield! subSeq
-    }
 
 
+let result_part1 (fileSystem: Bloc list) =
+    fileSystem
+    |> part1
+    |> countAll
+    |> Seq.toList
+    |> Seq.indexed
+    |> Seq.sumBy (fun (i, id) -> bigint id * bigint i)
 
-printfn
-    "%A"
-    (parse input2
-     |> part1
-     |> countAll
-     |> Seq.toList
-     |> Seq.indexed
-     |> Seq.sumBy (fun (i, id) -> bigint id * bigint i))
+let part2 (fileSystem: Bloc list) =
+    let reversed = (List.rev fileSystem)
+
+    let rec aux =
+        function
+        | [] -> []
+        // free blocs don't need to be reallocated
+        | (Free n) :: xs -> (Free n) :: aux xs
+        | (Filled(filledItem)) :: xs ->
+
+            let lastFreeSpaceLargeEnough =
+                Seq.tryFindBack
+                    (function
+                    | (_, Free(n)) when n >= filledItem.count -> true
+                    | _ -> false)
+                    (xs |> List.indexed)
+
+            match lastFreeSpaceLargeEnough with
+            | Some(index, Free(freeItem)) ->
+
+                let freeOrNothing =
+                    if (freeItem - filledItem.count) = 0 then
+                        []
+                    else
+                        [ Free(freeItem - filledItem.count) ]
+
+                let before = xs.[0 .. index - 1]
+                let after = xs.[index + 1 ..]
+
+                Free(filledItem.count)
+                :: aux (before @ freeOrNothing @ [ Filled filledItem ] @ after)
+            | _ -> Filled(filledItem) :: aux xs
+
+    aux reversed |> List.rev
+
+
+let part2TailRec (fileSystem: Bloc list) =
+    let reversed = List.rev fileSystem
+
+    let rec aux acc remaining =
+        match remaining with
+        | [] -> acc
+        // Free blocks are added to the accumulator as they are
+        | (Free n) :: xs -> aux (Free n :: acc) xs
+        | (Filled filledItem) :: xs ->
+            let lastFreeSpaceLargeEnough =
+                Seq.tryFindBack
+                    (function
+                    | (_, Free n) when n >= filledItem.count -> true
+                    | _ -> false)
+                    (xs |> List.indexed)
+
+            match lastFreeSpaceLargeEnough with
+            | Some(index, Free freeItem) ->
+                let freeOrNothing =
+                    if (freeItem - filledItem.count) = 0 then
+                        []
+                    else
+                        [ Free(freeItem - filledItem.count) ]
+
+                let before = xs.[0 .. index - 1]
+                let after = xs.[index + 1 ..]
+
+                // Accumulate the new Free block and recursively process the rest
+                aux (Free filledItem.count :: acc) (before @ freeOrNothing @ [ Filled filledItem ] @ after)
+            | _ ->
+                // If no suitable free space is found, keep the filled block
+                aux (Filled filledItem :: acc) xs
+
+    aux [] reversed
+
+
+let printFileSystem (fileSystem: Bloc list) =
+    let myseq =
+        seq {
+            for (bloc) in fileSystem do
+                let subSeq =
+                    match bloc with
+                    | Filled({ count = count; id = Id(id) }) -> Seq.replicate count (string id)
+                    | Free(n) -> Seq.replicate n "."
+
+                yield! subSeq
+        }
+
+    myseq |> Seq.iter (fun id -> printf "%s" id) |> (fun _ -> printfn "")
+
+
+let result_part2 (fileSystem: Bloc list) =
+    fileSystem
+    |> part2TailRec
+    |> countAll
+    |> Seq.toList
+    |> Seq.indexed
+    |> Seq.sumBy (fun (i, id) -> bigint id * bigint i)
+
+let fileSystem = parse input2
+
+printfn "Before"
+printfn "Part 2: %A" (result_part2 fileSystem)
